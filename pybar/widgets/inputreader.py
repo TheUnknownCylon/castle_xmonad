0e3a08f7b217bcb710924d44764d3c6510207939
TheUC <theunknowncylon@live.nl>
2017-09-19 21:10:53 +0200

    Squashed 'home/programs/pybar/' changes from b71de63..9d61997
    9d61997 PyQt4 -> PyQt5
    8c792de Merge pull request #1 from TheUnknownCylon/development
    d3f97d1 Docs and style improvements
    fb32365 Improved code readability.
    6a2e1ea Python2&Python3 compatability
    
    git-subtree-dir: home/programs/pybar
    git-subtree-split: 9d61997e706a94aa2742653c5bdd4faa7f926cd7

--- pybar/widgets/inputreader.py
+++ pybar/widgets/inputreader.py
@@ -1,9 +1,9 @@
 import sys
 from pybar import Widget
 
+
 class InputReader(Widget):
-    '''Simple widget that takes a Dzen2 formatted string as input, and draws
-    it on the bar.'''
+    '''Simple widget that reads and draws Dzen2 formatted strings.'''
 
     def setup(self):
         self.update()
@@ -16,4 +16,3 @@ class InputReader(Widget):
         while line:
             self.value(line.rstrip("\n"))
             line = sys.stdin.readline()
-
